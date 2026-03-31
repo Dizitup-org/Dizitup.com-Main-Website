56c526f2c94ca34063f7c5f91141b1d16af6fe6a
d_9ght <researchforhealthcare@gmail.com>
2026-03-31 14:20:12 +0530

    nodemail

--- backend/dizitup-backend/src/utils/mailer.js
+++ backend/dizitup-backend/src/utils/mailer.js
@@ -10,4 +10,13 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+// Verify SMTP connection on startup correctly
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('❌ SMTP Connection Error:', error);
+  } else {
+    console.log('🚀 SMTP Server is ready to take our messages');
+  }
+});
+
 module.exports = transporter;
